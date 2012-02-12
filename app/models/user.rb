7f6fa54b1971d20c4330604ffba9893821a47cd8
Eddie Vlagea <xtzyonweb@gmail.com>
2012-02-12 17:43:14 +0200

    added small validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+	has_many :books
+	#validate :user, :length => {:minimum => 2}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Bookrepository::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'users#index'
+   root :to => 'books#index'
 
   # See how all your routes lay out with "rake routes"
 
